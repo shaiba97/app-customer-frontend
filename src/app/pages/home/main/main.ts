52da66690a1b47283b3297e0af9667aa820e3eb6
shaiba97 <hsnshaiba17@gmail.com>
2026-07-22 04:44:39 +0530

    fix-restore-desktop-home-marketing-sections

--- src/app/pages/home/main/main.ts
+++ src/app/pages/home/main/main.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-import { TripListComponent } from '../trip-list/trip-list';
 import { SearchHeroComponent } from '../search-hero/search-hero';
+import { HowItWorksComponent } from '../sections/how-it-works/how-it-works.component';
+import { WhyTafiyaComponent } from '../sections/why-tafiya/why-tafiya.component';
+import { AppDownloadComponent } from '../sections/app-download/app-download.component';
+import { BlogCarouselComponent } from '../sections/blog-carousel/blog-carousel.component';
+import { BottomCtaComponent } from '../sections/bottom-cta/bottom-cta.component';
+import { HomeFooterComponent } from '../sections/home-footer/home-footer.component';
+import { TripListComponent } from '../trip-list/trip-list';
 
 @Component({
   selector: 'app-main',
-  imports: [TripListComponent, SearchHeroComponent],
+  imports: [
+    SearchHeroComponent,
+    HowItWorksComponent,
+    WhyTafiyaComponent,
+    AppDownloadComponent,
+    BlogCarouselComponent,
+    BottomCtaComponent,
+    HomeFooterComponent,
+    TripListComponent,
+  ],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
